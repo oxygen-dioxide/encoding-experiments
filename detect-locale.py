a2c9cfb67a98180e71df375d497e10d528a6845d
oxygen-dioxide <54425948+oxygen-dioxide@users.noreply.github.com>
2023-09-03 11:02:29 +0800

    a

--- detect-locale.py
+++ detect-locale.py
@@ -1,18 +1,38 @@
+import json
 import win32api
 import yaml
 
+
+#load lcid.json
+with open('lcid.json', 'r', encoding="utf-8") as f:
+    lcid = json.load(f)
+
+
+
+KeyboardLayout = win32api.GetKeyboardLayout()
+KeyboardLayoutList = list(win32api.GetKeyboardLayoutList())
+KeyboardLayoutName = win32api.GetKeyboardLayoutName()
+SystemDefaultLangID = win32api.GetSystemDefaultLangID()
+SystemDefaultLangTag = lcid[str(SystemDefaultLangID)]
+SystemDefaultLCID = win32api.GetSystemDefaultLCID()
+ThreadLocale = win32api.GetThreadLocale()
+ThreadLocaleTag = lcid[str(ThreadLocale)]
+UserDefaultLangID = win32api.GetUserDefaultLangID()
+UserDefaultLangTag = lcid[str(UserDefaultLangID)]
+UserDefaultLCID = win32api.GetUserDefaultLCID()
+
 result={
-    #"GetSystemDefaultUILanguage": win32api.GetSystemDefaultUILanguage(),
-    #"GetUserDefaultUILanguage": win32api.GetUserDefaultUILanguage(),
-    #"GetThreadUILanguage": win32api.GetThreadUILanguage(),
-    "GetKeyboardLayout": win32api.GetKeyboardLayout(),
-    "GetKeyboardLayoutList": list(win32api.GetKeyboardLayoutList()),
-    "GetKeyboardLayoutName": win32api.GetKeyboardLayoutName(),
-    "GetSystemDefaultLangID": win32api.GetSystemDefaultLangID(),
-    "GetSystemDefaultLCID": win32api.GetSystemDefaultLCID(),
-    "GetThreadLocale": win32api.GetThreadLocale(),
-    "GetUserDefaultLangID": win32api.GetUserDefaultLangID(),
-    "GetUserDefaultLCID": win32api.GetUserDefaultLCID(),
+    "KeyboardLayout": KeyboardLayout,
+    "KeyboardLayoutList": KeyboardLayoutList,
+    "KeyboardLayoutName": KeyboardLayoutName,
+    "SystemDefaultLangID": SystemDefaultLangID,
+    "SystemDefaultLangTag": SystemDefaultLangTag,
+    "SystemDefaultLCID": SystemDefaultLCID,
+    "ThreadLocale": ThreadLocale,
+    "ThreadLocaleTag": ThreadLocaleTag,
+    "UserDefaultLangID": UserDefaultLangID,
+    "UserDefaultLangTag": UserDefaultLangTag,
+    "UserDefaultLCID": UserDefaultLCID,
 }
 
 print(yaml.dump(result))
