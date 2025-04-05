e0b39e49f70162bfc9f5a6909b4e954f6e634435
unknown <1463567152@qq.com>
2025-04-05 20:37:05 +0800

    add csharp

---
+++ csharp/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Text;
+
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+var a=Encoding.GetEncoding(0);
+Console.WriteLine(a.EncodingName);
+Console.WriteLine(a.WebName);
+Console.WriteLine(a.CodePage);
